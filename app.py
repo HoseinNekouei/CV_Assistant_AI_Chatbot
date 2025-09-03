f70e43e5c024364aea0e89ca1161a01e566b359f
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-09-03 14:53:35 +0330

    updated chromaDB setting for sqlite <= 35

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import streamlit as st
 from pydantic import SecretStr
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
+from chromadb.config import Settings
 from langchain_chroma import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import HumanMessage, AIMessage
@@ -146,12 +147,16 @@ class VectorStoreCache:
         self.collection = None
 
     def load_or_create(self, text_chunks: List[str]):
+        # Initialize Chroma client with DuckDB backend
+        client_settings = Settings(chroma_db_impl="duckdb+parquet")
+        
         # Load Existing ChromaDB or create a new with only missing chunks
         if os.path.exists(self.persist_dir) and os.listdir((self.persist_dir)):
             #load existing collection
             self.collection= Chroma(
                 embedding_function= self.embedding_model,
-                persist_directory= self.persist_dir
+                persist_directory= self.persist_dir,
+                client_settings=client_settings 
             )
 
         else:
@@ -160,7 +165,8 @@ class VectorStoreCache:
                 self.collection= Chroma.from_texts(
                     texts= text_chunks,
                     embedding=self.embedding_model,
-                    persist_directory=self.persist_dir
+                    persist_directory=self.persist_dir,
+                    client_settings=client_settings
                     )
                                 
                 return self.collection
